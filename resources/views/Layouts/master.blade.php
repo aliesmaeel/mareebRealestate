b897c78e1f46dde03336d7661bfba29324b6fac9
Ali Esmaeel <esmaeel@mobilapp.io>
2024-10-22 22:20:57 +0200

    change main color for page

--- resources/views/Layouts/master.blade.php
+++ resources/views/Layouts/master.blade.php
@@ -26,7 +26,6 @@
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="{{asset('js/app.js?v=0.8')}}"></script>
     <script src="{{asset('js/aos.js')}}"></script>
-    <script src="{{asset('js/particles.js')}}"></script>
     <script>
         AOS.init();
     </script>
