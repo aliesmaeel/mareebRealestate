bb292814a265d4a40247e3ddbfccf46e7097e321
Ali Esmaeel <esmaeel@mobilapp.io>
2024-10-30 05:56:01 +0100

    change title

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>MeerabREALESTATE | @yield('title')</title>
+    <title>Meerab Properties  | @yield('title')</title>
     <link rel="icon" href="" type="image/icon type">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flag-icons/css/flag-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
