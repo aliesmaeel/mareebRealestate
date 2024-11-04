ced50f2726b9dc30dd582fb732356bc5a7e9170e
Ali Esmaeel <esmaeel@mobilapp.io>
2024-11-04 09:54:29 +0100

    deploy

--- app/Models/Footer.php
+++ app/Models/Footer.php
@@ -6,10 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Footer extends Model
 {
-    protected $fillable=['image','text','links','copy_write','active','created_at'];
+    protected $fillable=['image','text','links','copy_write','active'];
 
     protected $casts = [
-
         'links' => 'json',
     ];
 
