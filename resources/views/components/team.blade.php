32c4f10952fed84b38c9f12081973b2c01bc9863
Ali Esmaeel <esmaeel@mobilapp.io>
2024-12-07 23:36:23 +0100

    change main color in all pages

--- resources/views/components/team.blade.php
+++ resources/views/components/team.blade.php
@@ -1,10 +1,8 @@
 <div class="col flex-col">
+
     <div class="img-container">
         <img src="{{asset('/storage/'.$team->image)}}">
         <div class="overlay">
-            <div>
-                <a href="#">View profile</a>
-            </div>
         </div>
     </div>
     <a class="name" href="#">{{$team->name}}</a>
