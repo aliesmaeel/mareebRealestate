6ef55cf62c66b1002d21ff450a6b0a3511b1d504
Ali Esmaeel <esmaeel@mobilapp.io>
2024-12-13 15:58:23 +0100

    add bootstrap to our-team

--- resources/views/our_team.blade.php
+++ resources/views/our_team.blade.php
@@ -7,6 +7,13 @@
 @endsection
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 {{-- main page --}}
+<style>
+    @media screen and (min-width: 1600px) {
+        .container {
+            max-width: 1416px;
+        }
+    }
+</style>
 @section('content')
 
     <div class="container team">
