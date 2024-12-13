fd00cfc0b25129aab36f4efaaea74df88a8da969
Ali Esmaeel <esmaeel@mobilapp.io>
2024-12-13 15:23:10 +0100

    add bootstrap to our-team

--- resources/views/our_team.blade.php
+++ resources/views/our_team.blade.php
@@ -5,7 +5,7 @@
 @section('header')
     @include('layouts.header')
 @endsection
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 {{-- main page --}}
 @section('content')
 
@@ -22,7 +22,6 @@
                     @endforeach
                 </div>
             </div>
-
         </div>
     </div>
 @endsection
